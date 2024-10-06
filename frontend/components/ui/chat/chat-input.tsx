566e0a83d2d38017f1ba61042acdb1d518ffc2ca
Noah Solomon <noahsolomon2003@gmail.com>
2024-10-06 17:10:19 -0400

    stuff

--- frontend/components/ui/chat/chat-input.tsx
+++ frontend/components/ui/chat/chat-input.tsx
@@ -1,21 +1,21 @@
 import * as React from "react";
-import { Textarea } from "@/components/ui/textarea";
 import { cn } from "@/lib/utils";
+import { TextArea } from "frosted-ui";
 
 interface ChatInputProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
 const ChatInput = React.forwardRef<HTMLTextAreaElement, ChatInputProps>(
   ({ className, ...props }, ref) => (
-    <Textarea
-      autoComplete="off"
-      ref={ref}
-      name="message"
+    <TextArea
+      value={props.value}
+      placeholder={props.placeholder}
+      variant="surface"
+      onChange={props.onChange}
       className={cn(
-        "max-h-12 px-4 py-3 bg-background text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 w-full rounded-md flex items-center h-16 resize-none",
+        "max-h-12 bg-background text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 w-full rounded-md flex items-center h-16 resize-none",
         className
       )}
-      {...props}
     />
   )
 );
