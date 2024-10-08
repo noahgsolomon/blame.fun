ca9fc31a24d4fcf53aa9efc53b684c821dc2c735
Noah Solomon <noahsolomon2003@gmail.com>
2024-10-07 22:05:27 -0400

    page uses header

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,25 @@
-import { Textarea } from "@/components/ui/textarea";
 import { Button } from "frosted-ui";
 import Chat from "./chat";
+import Header from "./header";
 
-export default function Home() {
+export default async function Page() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <div className="flex flex-col gap-2 items-center">
-        <Button variant="classic" color="purple" style={{ cursor: "pointer" }}>
-          sup
-        </Button>
-        <Chat />
+    <div className="h-screen ">
+      <Header />
+      <div className="flex h-[75%] items-center justify-center">
+        <div className="flex flex-col gap-2 items-center">
+          <h1>FRONTEND</h1>
+          <a href="http://localhost:3000">
+            <Button
+              variant="classic"
+              color="purple"
+              style={{ cursor: "pointer" }}
+            >
+              go to rails
+            </Button>
+          </a>
+          <Chat />
+        </div>
       </div>
     </div>
   );
