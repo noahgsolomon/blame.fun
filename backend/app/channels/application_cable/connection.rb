f0b73a5f5c3d4db6a2d68b165ca722373bca9004
Noah Solomon <noahsolomon2003@gmail.com>
2024-10-10 00:46:36 -0400

    connection

--- backend/app/channels/application_cable/connection.rb
+++ backend/app/channels/application_cable/connection.rb
@@ -1,4 +1,21 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      token = request.params[:token]
+      user_id = Rails.cache.read("ws_auth_#{token}")
+      if user_id
+        User.find(user_id)
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
