8c33a90be9fd44aaa8aef1cbbb515b6c2cc7366c
Noah Solomon <noahsolomon2003@gmail.com>
2024-10-10 00:47:57 -0400

    header ui

--- frontend/app/header.tsx
+++ frontend/app/header.tsx
@@ -7,23 +7,24 @@ import {
   DropdownMenuTrigger,
 } from "frosted-ui";
 import { useUserStore } from "./stores/user-store";
-import { userInfo } from "os";
 
 export default function Header() {
   const { user } = useUserStore();
   return (
-    <div className="w-full flex justify-end p-4">
+    <div className=" w-full flex justify-end p-4">
       <DropdownMenuRoot>
         <DropdownMenuTrigger>
           <Avatar
             className="cursor-pointer"
-            src="/logo.png"
+            src={user?.image}
             alt="icon"
             fallback="N"
           />
         </DropdownMenuTrigger>
         {user && user.name && (
-          <DropdownMenuContent>{user.name}</DropdownMenuContent>
+          <DropdownMenuContent>
+            <div className="font-jetbrains">{user.name}</div>
+          </DropdownMenuContent>
         )}
       </DropdownMenuRoot>
     </div>
