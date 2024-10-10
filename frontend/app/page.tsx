b79f4504f17ca3c58dab9cd803844e660d0e33d4
Noah Solomon <noahsolomon2003@gmail.com>
2024-10-10 00:48:17 -0400

    main app ui

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,25 +1,12 @@
-import { Button } from "frosted-ui";
-import Chat from "./chat";
-import Header from "./header";
+import UserEnvironments from "./user-environments";
+import CreateEnvironment from "./create-environment";
 
 export default async function Page() {
   return (
-    <div className="h-screen ">
-      <Header />
-      <div className="flex h-[75%] items-center justify-center">
-        <div className="flex flex-col gap-2 items-center">
-          <h1>FRONTEND</h1>
-          <a href="http://localhost:3000">
-            <Button
-              variant="classic"
-              color="purple"
-              style={{ cursor: "pointer" }}
-            >
-              go to rails
-            </Button>
-          </a>
-          <Chat />
-        </div>
+    <div className="flex h-[75%] items-center justify-center">
+      <div className="flex flex-col gap-2 items-center">
+        <CreateEnvironment />
+        <UserEnvironments />
       </div>
     </div>
   );
