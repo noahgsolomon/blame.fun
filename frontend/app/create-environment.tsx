0ca1fb41cd9098e99b93174965e9b85ef83f9a6c
Noah Solomon <noahsolomon2003@gmail.com>
2024-10-10 00:47:49 -0400

    create env

---
+++ frontend/app/create-environment.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Button } from "frosted-ui";
+import { Loader, Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "sonner";
+
+export default function CreateEnvironment() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  async function createEnvironment() {
+    try {
+      setLoading(true);
+      const response = await fetch("http://localhost:3000/environments", {
+        method: "POST",
+        credentials: "include",
+      });
+      const data = await response.json();
+      if (data.environmentId) {
+        localStorage.setItem("userId", data.userId);
+        router.push(`/environment/${data.environmentId}`);
+      }
+    } catch (error) {
+      console.error("Failed to create environment", error);
+      toast.error(`Internal server error`);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <Button
+      disabled={loading}
+      onClick={createEnvironment}
+      variant="soft"
+      color="purple"
+      style={{ cursor: "pointer" }}
+      className="w-full"
+    >
+      {loading ? (
+        <Loader className="size-4 animate-spin" />
+      ) : (
+        <>
+          <Plus className="size-4" /> create environment
+        </>
+      )}
+    </Button>
+  );
+}
