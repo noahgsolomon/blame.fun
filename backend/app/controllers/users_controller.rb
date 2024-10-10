a37953a176aca1552794d43ec7d6306bcd244b82
Noah Solomon <noahsolomon2003@gmail.com>
2024-10-10 00:46:50 -0400

    user controller

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,21 +1,7 @@
 class UsersController < ApplicationController
-  def fetch_or_create
-    token = cookies[:browser_token]
-    puts token
+  include UserFetcher
 
-    if token && (user = User.find_by(browser_token: token))
-      render json: {user: user}
-    else
-      new_token = SecureRandom.uuid
-      user = User.create!(browser_token: new_token, name: 'sussy snake')
-      response.set_cookie(:browser_token, {
-        value: new_token,
-        expires: 10.years.from_now,
-        httponly: true,
-        secure: Rails.env.production?,
-        same_site: :lax
-      })
-      render json: {user: user}
-    end
+  def index
+    render json: { user: @current_user }
   end
 end
