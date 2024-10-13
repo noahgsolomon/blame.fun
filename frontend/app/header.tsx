77e77852278e0e3167a21c4f926e30d35779791b
Noah Solomon <noahsolomon2003@gmail.com>
2024-10-13 11:39:56 -0400

    shad stfuf

--- frontend/app/header.tsx
+++ frontend/app/header.tsx
@@ -1,32 +1,30 @@
 "use client";
 
-import {
-  Avatar,
-  DropdownMenuContent,
-  DropdownMenuRoot,
-  DropdownMenuTrigger,
-} from "frosted-ui";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useUserStore } from "./stores/user-store";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Header() {
   const { user } = useUserStore();
   return (
-    <div className=" w-full flex justify-end p-4">
-      <DropdownMenuRoot>
-        <DropdownMenuTrigger>
-          <Avatar
-            className="cursor-pointer"
-            src={user?.image ?? ""}
-            alt="icon"
-            fallback="N"
-          />
+    <div className="w-full flex justify-end p-4">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Avatar className="cursor-pointer focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2">
+            <AvatarImage src={user?.image ?? ""} />
+            <AvatarFallback>{user?.name?.[0]}</AvatarFallback>
+          </Avatar>
         </DropdownMenuTrigger>
         {user && user.name && (
           <DropdownMenuContent>
             <div className="font-jetbrains">{user.name}</div>
           </DropdownMenuContent>
         )}
-      </DropdownMenuRoot>
+      </DropdownMenu>
     </div>
   );
 }
