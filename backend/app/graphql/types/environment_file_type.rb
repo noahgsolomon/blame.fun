fb0ee7f801672bbc2432dfdd9d553a4e111ab664
Noah Solomon <noahsolomon2003@gmail.com>
2024-10-15 21:14:18 -0400

    adding environment file type and environmet file to environment type

--- backend/app/graphql/types/environment_file_type.rb
+++ backend/app/graphql/types/environment_file_type.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
 module Types
-  class EnvironmentType < Types::BaseObject
+  class EnvironmentFileType < Types::BaseObject
     field :id, ID, null: false
-    field :name, String
+    field :environment_id, Integer, null: false
+    field :filename, String
+    field :content, String
+    field :file_extension, String
+    field :file_size, Integer
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :environment_files, [Types::EnvironmentFileType], null: false
-
-    def environment_files
-      object.environment_files
-    end
   end
 end
