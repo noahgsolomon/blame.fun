12a8960dc77445d2807f868c3dd913c374985214
Noah Solomon <noahsolomon2003@gmail.com>
2024-10-16 20:43:09 -0400

    dont need environments controller anymore

--- backend/app/graphql/types/mutation_type.rb
+++ backend/app/graphql/types/mutation_type.rb
@@ -2,6 +2,8 @@
 
 module Types
   class MutationType < Types::BaseObject
+    field :join_environment, mutation: Mutations::JoinEnvironment
+    field :create_environment, mutation: Mutations::CreateEnvironment
     field :add_new_file, mutation: Mutations::AddNewFile
     field :create_invite_link, mutation: Mutations::CreateInviteLink
     field :delete_environment_file, mutation: Mutations::DeleteEnvironmentFile
