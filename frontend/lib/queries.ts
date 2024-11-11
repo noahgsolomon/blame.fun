65d1012f4e695f1fecf75c626bbda9434b9fc2bb
Noah Solomon <noahsolomon2003@gmail.com>
2024-11-11 16:14:58 -0500

    GET_REPOSITORY

--- frontend/lib/queries.ts
+++ frontend/lib/queries.ts
@@ -1,14 +1,32 @@
 import { gql } from "@apollo/client";
 
 export const GET_REPOSITORY = gql`
-  query GetRepository($username: String!, $slug: String!) {
+  query GetRepository($username: String!, $slug: String!, $path: String = "") {
     repositories(username: $username, slug: $slug) {
       id
       name
       description
       slug
+      gitUrl
       createdAt
       updatedAt
+      tree(path: $path) {
+        name
+        type
+        oid
+        path
+      }
+      treeEntryDetails(path: $path) {
+        path
+        type
+        file
+        size
+        msg
+        name
+        date
+        oid
+      }
+      fileContent(path: $path)
     }
   }
 `;
