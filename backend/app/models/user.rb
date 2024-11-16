13e99865890b8a2940335714e05f8653fae7d676
Noah Solomon <noahsolomon2003@gmail.com>
2024-11-15 21:10:20 -0500

    follower following user model derived fields

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -9,8 +9,19 @@ class User < ApplicationRecord
   
   has_many :stars
   
+  has_many :follows_as_follower, class_name: 'Follow', foreign_key: :follower_id
+  has_many :follows_as_followed, class_name: 'Follow', foreign_key: :followed_id
+  
+  has_many :following, through: :follows_as_follower, source: :followed
+  has_many :followers, through: :follows_as_followed, source: :follower
+
   def self.authenticate(username, password)
     user = find_by(username: username)
     user&.authenticate(password)
+  end
+
+  def followed_by?(user)
+    return false unless user
+    followers.include?(user)
   end
 end
